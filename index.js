9348c79906b5a36782a5bf6ce8a2610bfa19f9b5
Gavin King <gavin.king@itslearning.com>
2017-07-22 01:03:37 +0200

    remove fluff

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 const config    = require('config');
 const mysql     = require('promise-mysql');
 
-// mysql.createConnection(config.get('db'))
-//     .then(conn => connection = conn)
-//     .then((response) => console.log(response))
-//     .finally(() => connection.end());
-
-// mysql.createConnection(config.get('db'))
-//     .then(conn => connection = conn)
-//     .then(() => connection.query('DESCRIBE titles'))
-//     .then((response) => console.log(response))
-//     .finally(() => connection.end());
-
 function create() {
     const query = 'INSERT INTO titles SET ?';
     const params = {'title': 'I am a title'};
@@ -39,5 +28,3 @@ function update() {
         .then(results => console.log(results))
         .finally(() => connection.end());
 }
-
-update()
